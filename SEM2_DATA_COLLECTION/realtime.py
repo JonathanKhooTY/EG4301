5c7bd49afa4e975645571177dab4dcb38d4e5db2
JonathanKhooTY <jonathan.khooty@gmail.com>
2023-02-23 14:18:02 +0800

    Edited plot limits and autoscroll

--- SEM2_DATA_COLLECTION/realtime.py
+++ SEM2_DATA_COLLECTION/realtime.py
@@ -12,15 +12,16 @@ sensors = [['LU','MU','RU'],
 ['LB','MB','RB']]
 
 # Set up plot
-fig, ax = plt.subplots(3, 3, figsize=(20, 10))
+fig, ax = plt.subplots(3, 3, figsize=(20, 15))
 lines = []
 for i in range(3):
     for j in range(3):
         line, = ax[i, j].plot([], [])
         lines.append(line)
-        ax[i, j].set_xlim(0, 100)  #Desired x-axis limit
-        ax[i, j].set_ylim(0, 1023)  # Max value of your sensor
+        ax[i, j].set_xlim(0, 60)  #Desired x-axis limit
+        ax[i, j].set_ylim(0, 800)  # Max value of your sensor
         ax[i, j].set_title(sensors[i][j])
+        ax[i, j].grid('true','both')
         #ax[i, j].set_title(f"Sensor {i*3+j+1}")
 
 # Define function to update plot
@@ -30,7 +31,7 @@ def update_plot(frame):
         data = [int(x) for x in data]
         for i, line in enumerate(lines):
             line.set_data(np.arange(len(line.get_xdata())+1), np.append(line.get_ydata(), data[i]))
-            if len(line.get_xdata()) > 100:
+            if len(line.get_xdata()) > 60:
                 line.set_xdata(line.get_xdata()[1:])
                 line.set_ydata(line.get_ydata()[1:])
                 ax[i//3, i%3].set_xlim(line.get_xdata()[0], line.get_xdata()[-1])
